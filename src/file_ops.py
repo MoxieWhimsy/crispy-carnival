fbc138aea4f37864ed6a4e3c320058c0807fbd62
Paul Ping Kohler <paul@fantasmacollective.com>
2026-06-20 12:40:13 -0400

    add clear_and_copy function; use it to copy static to public
    bonus: ignore public folder and remove and stop tracking its contents in repo

---
+++ src/file_ops.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+
+def clear_and_copy(source: str, destination: str):
+    if not os.path.isdir(source):
+        raise TypeError("Source must be a directory")
+    if not os.path.isdir(destination):
+        shutil.rmtree(destination, ignore_errors=True)
+    os.mkdir(destination)
+    contents = os.listdir(source)
+    if contents is None or len(contents) == 0:
+        return
+    directories = filter(lambda x: os.path.isdir(os.path.join(source, x)), contents)
+    files = filter(lambda x: os.path.isfile(os.path.join(source, x)), contents)
+    for directory in directories:
+        directory_path = os.path.join(destination, directory)
+        clear_and_copy(os.path.join(source, directory), directory_path)
+    for file in files:
+        shutil.copy(os.path.join(source, file), destination)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
+from file_ops import clear_and_copy
 from textnode import TextType, TextNode
 
+clear_and_copy('static', 'public')
 print("hello world")
 
 dummy = TextNode("This is a dummy node", TextType.LINK, "https://www.boot.dev")
