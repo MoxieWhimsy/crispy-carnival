3b496e73e3698e4276a0e9928077c754c9c1835f
Paul Ping Kohler <paul@fantasmacollective.com>
2026-06-20 16:17:14 -0400

    add generate page, content from course, and start http server in main.sh

--- src/file_ops.py
+++ src/file_ops.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 
+from markdown_parser import markdown_to_html_node, extract_title
+
 
 def clear_and_copy(source: str, destination: str):
     if not os.path.isdir(source):
@@ -17,3 +19,22 @@ def clear_and_copy(source: str, destination: str):
         clear_and_copy(os.path.join(source, directory), directory_path)
     for file in files:
         shutil.copy(os.path.join(source, file), destination)
+
+def generate_page(from_path: str, template_path: str, dest_path: str):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    if not os.path.isfile(from_path):
+        raise TypeError("from_path must be a file")
+    if not os.path.isfile(template_path):
+        raise TypeError("template must be a file")
+    file = open(from_path, "r")
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    content = file.read()
+    file.close()
+    template_file.close()
+    content_html_node = markdown_to_html_node(content)
+    content_html = content_html_node.to_html()
+    title = extract_title(content)
+    full_html = template.replace("{{ Content }}", content_html).replace("{{ Title }}", title)
+    with open(dest_path, "w") as f:
+        f.write(full_html)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from file_ops import clear_and_copy
+from file_ops import clear_and_copy, generate_page
 from textnode import TextType, TextNode
 
 clear_and_copy('static', 'public')
+generate_page('content/index.md', 'template.html', 'public/index.html')
+
 print("hello world")
 
 dummy = TextNode("This is a dummy node", TextType.LINK, "https://www.boot.dev")
